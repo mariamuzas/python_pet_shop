0c4215817a37f09d05110dca4ae9ea21e3dd6d99
mmuzas <mmuzasantuna@gmail.com>
2020-09-11 18:05:29 +0100

    Functions for shop name and cash added

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,1 +1,7 @@
 # WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name(pet_shop_name):
+    return pet_shop_name["name"]
+
+def get_total_cash (shop_cash):
+    return shop_cash["admin"]["total_cash"]
