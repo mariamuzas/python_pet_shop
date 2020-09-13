98b17731f15b48eeaacc411714478f1e26d69084
mmuzas <mmuzasantuna@gmail.com>
2020-09-13 10:58:52 +0100

    add_or_remove_cash, pet_sold and get_stock_count functions added.

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -3,5 +3,28 @@
 def get_pet_shop_name(pet_shop_name):
     return pet_shop_name["name"]
 
-def get_total_cash (shop_cash):
+
+def get_total_cash(shop_cash):
     return shop_cash["admin"]["total_cash"]
+
+
+def add_or_remove_cash(shop_cash, add_cash):
+    shop_cash["admin"]["total_cash"] += add_cash
+
+
+def add_or_remove_cash(shop_cash, remove_cash):
+    shop_cash["admin"]["total_cash"] += remove_cash
+
+
+def get_pets_sold(shop_cash):
+    return shop_cash["admin"]["pets_sold"]
+
+
+def increase_pets_sold(pet_sold, num_pets):
+    pet_sold["admin"]["pets_sold"] += num_pets
+
+
+def get_stock_count(store_pets):
+    return len(store_pets["pets"])
+
+
