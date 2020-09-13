98725930999c8594cd4641f8c9272772308b30fa
mmuzas <mmuzasantuna@gmail.com>
2020-09-13 13:54:29 +0100

    add_pet_to_customer function added. MVP done

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -31,25 +31,36 @@ def get_pets_by_breed(store_pets, name_breed):
             count_breed.append(pet["breed"])
     return count_breed
 
+
 def find_pet_by_name(store_pets, name_pet):
     for pet in store_pets["pets"]:
         if pet["name"] == name_pet:
             return pet
+
 
 def remove_pet_by_name(store_pets, name_pet):
     for pet in store_pets["pets"]:
         if pet["name"] == name_pet:
             store_pets["pets"].remove(pet)
 
+
 def add_pet_to_stock(store_pets, new_pet):
     new_pet == {}
     store_pets["pets"].append(new_pet)
     
+
 def get_customer_cash(list_customers):
     return list_customers["cash"]
+
 
 def remove_customer_cash(list_customers, customer_cash):
     list_customers["cash"] -= customer_cash
 
+
 def get_customer_pet_count(list_customers):
-        return len(list_customers["pets"])
+    return len(list_customers["pets"])
+
+
+def add_pet_to_customer(name_customer, new_pet):
+    return name_customer["pets"].append(new_pet)
+
